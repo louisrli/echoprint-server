cf17c99d470e8e227d0087cb7d5c00de44d95953
Louis Li <louisrli@gmail.com>
2013-08-03 15:48:47 -0400

    Working version of queries

--- API/api.py
+++ API/api.py
@@ -11,6 +11,7 @@ from __future__ import with_statement
 import web
 import fp
 import re
+import youtube as yt
 
 try:
     import json
@@ -23,52 +24,84 @@ except ImportError:
 urls = (
     '/query', 'query',
     '/query?(.*)', 'query',
-    '/ingest', 'ingest',
+    '/youtube', 'youtube'
 )
 
 
-class ingest:
+CODEVER = 4.21
+
+class youtube:
     def POST(self):
-        params = web.input(fp_code="", artist=None, track=None, length=None, codever=None)
+        params = web.input(youtube_url=None, start=None, stop=None)
 
-        track_id = params.track_id
-        CODEVER = 4.21
-
-        if params.length is None or params.codever is None:
+        if not (params.youtube_url and params.start and params.stop):
             return web.webapi.BadRequest()
-        
-        # First see if this is a compressed code
-        if re.match('[A-Za-z\/\+\_\-]', params.fp_code) is not None:
-           code_string = fp.decode_code_string(params.fp_code)
-           if code_string is None:
-               return json.dumps({"track_id":track_id, "ok":False, "error":"cannot decode code string %s" % params.fp_code})
-        else:
-            code_string = params.fp_code
 
-        data = {"track_id": track_id, 
-                "fp": code_string,
-                "length": params.length,
-                "codever": CODEVER }
+        # Extract the fingerprints from the videos
+        tag = yt.fingerprint_youtube(params.youtube_url, 
+                int(params.start), int(params.stop))
 
-        if params.artist: 
-            data["artist"] = params.artist
-        if params.track: 
-            data["track"] = params.track
+        # Query the tag in the database
+        best_match = _query_tag(tag['code'], tag)
+        return best_match
 
-        fp.ingest(data, do_commit=True, local=False)
-
-        return json.dumps({"track_id":track_id, "status":"ok"})
-        
-    
 class query:
     def POST(self):
-        return self.GET()
-        
-    def GET(self):
-        params = web.input(fp_code="")
-        response = fp.best_match_for_query(params.fp_code)
-        return json.dumps({"ok":True, "query":params.fp_code, "message":response.message(), "match":response.match(), "score":response.score, \
-                        "qtime":response.qtime, "track_id":response.TRID, "total_time":response.total_time})
+        params = web.input(mp3={})
+        if 'mp3' in params:
+            filename = _save_file(params.mp3, "audio")
+            tag = yt.generate_tag(filename)
+
+            print tag['code']
+            return _query_tag(tag['code'])
+        else:
+            return web.webapi.BadRequest()
+
+def _save_file(webfile, output_dir):
+    """
+    Saves a POSTed file to output_dir.
+
+    Args:
+        webfile: A web.py file object
+
+    Returns:
+        The path to the output file
+    """
+    filepath = webfile.filename.replace('\\','/') 
+    filename = filepath.split('/')[-1] 
+
+    o = output_dir +'/'+ filename
+
+    fout = open(o, 'w') 
+
+    try:
+        fout.write(webfile.file.read()) 
+    finally:
+        fout.close() 
+
+    return o
+
+
+def _query_tag(fp_code, query_obj=None):
+    """
+    Queries the database for given fingerprint
+
+    Args:
+        fp_code: A fingerprint string
+        query_obj: An optional dict of echoprint-codegen
+
+    """
+    response = fp.best_match_for_query(fp_code)
+    if response:
+        return json.dumps({
+            "ok":True, 
+            "message":response.message(),
+            "match":response.match(),
+            "score":response.score,
+            "qtime":response.qtime, 
+            "track_id":response.TRID, 
+            "total_time":response.total_time
+            })
 
 
 application = web.application(urls, globals())#.wsgifunc()
---
+++ API/youtube.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+import subprocess
+import pyechonest as pye
+import os
+import logging
+from pyechonest import config
+from pyechonest import song
+
+logger = logging.getLogger('youtube')
+logger.setLevel(logging.INFO)
+
+# EchoNest API configuation
+config.ECHO_NEST_API_KEY = "VVCRHMDOSJTUONGDQ"
+config.CODEGEN_BINARY_OVERRIDE = subprocess.check_output(["which", "echoprint-codegen"]).strip()
+config.TRACE_API_CALLS = True
+
+def fingerprint_youtube(url, start, stop, whole_video=False):
+    # Validate YouTube URL
+
+    mp3file = _download_audio(url)
+
+    tag = generate_tag(mp3file, start, stop)
+
+    if whole_video:
+        full_tag = generate_tag(mp3file)
+        return tag, full_tag
+    else:
+        return tag
+
+def _download_audio(url):
+    prog = "youtube-dl"
+    audio_dir = "audio"
+    flags = ["--extract-audio", 
+            "--max-quality", "5", 
+            "-o", audio_dir + "/" + "%(id)s.%(ext)s",
+            "--newline",  # progress bar on newlines
+            url] 
+
+    # Get the id for validation
+    id = subprocess.check_output([prog, "--get-id", url]).strip()
+    filename = "%s/%s.mp3" % (audio_dir, id)
+
+    if os.path.exists(filename):
+        logger.info("Already have the file: %s" % filename)
+        return filename
+
+    # Extract the mp3 file
+    p = subprocess.Popen([prog] + flags,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+
+    for line in iter(p.stdout.readline, b''):
+        print(">>> " + line.rstrip())
+
+    # Process the .mp3 file
+    return filename
+
+def generate_tag(filename, start=-1, stop=-1):
+    """
+    Note that these default arguments are due to the undocumented
+    way that the pyechonest library calls codegen, which checks
+    if start / duration are >= 0.
+
+    Args:
+        filename: The name of the input audio file to be identified
+        start: The start time, in seconds
+        stop: The stop time, in seconds
+    Returns:
+        A dict resulting from the echonest JSON
+    """
+    if (start > 0 and stop > 0) and not stop > start:
+        raise "TODO error"
+
+    duration = stop - start
+    codegen = pye.util.codegen(filename, start, duration)
+
+    if any(('error' in c) for c in codegen):
+        print codegen
+        raise Exception("Couldn't decode file")
+
+    # Only return the first result
+    return codegen[0]
+
+def identify_song(tag):
+    """
+    Uses the Echonest API to try identifying a song
+    Returns:
+        None if no results were found from any hosts
+        Returns a list of artist + title names if successful
+    """
+    result = song.identify(query_obj=tag)
+    if result:
+        return [{
+            'artist': r.artist_name,
+            'title': r.title
+            } for r in result]
+    else: 
+        return None
+
+if __name__ == '__main__':
+    main()
